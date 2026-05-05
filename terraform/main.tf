5afa92f759ddf95944352f39da4bafcb51dfd8e0
SrushtyP <srushtyshekhar@gmail.com>
2026-05-05 16:19:35 +0530

    fix: remove remote backend, add inventory.yml

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,4 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "srushtydfstate"
-    container_name       = "tfstate"
-    key                  = "dashboard.tfstate"
-  }
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
