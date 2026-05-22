61be7e9f02bda75d5f9747e04b1e00dd3a8012f5
Naik <srushty.naik@nouryon.com>
2026-05-22 11:12:50 +0530

    Fixed changes

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,24 +1,14 @@
 output "vm_running_ip" {
-  description = "Public IP of vm-running (app deployed here)"
+  description = "Public IP of vm-running"
   value       = azurerm_public_ip.pip_running.ip_address
 }
 
 output "vm_snoozed_ip" {
-  description = "Public IP of vm-snoozed (deallocated)"
+  description = "Public IP of vm-snoozed"
   value       = azurerm_public_ip.pip_snoozed.ip_address
 }
 
 output "vm_destroyed_ip" {
-  description = "Public IP of vm-destroyed (only when provisioned)"
-  value       = var.create_vm_destroyed ? azurerm_public_ip.pip_destroyed[0].ip_address : "not provisioned"
-}
-
-output "app_url" {
-  description = "URL to access the Flask app"
-  value       = "http://${azurerm_public_ip.pip_running.ip_address}:5000"
-}
-
-output "ssh_command" {
-  description = "SSH into vm-running"
-  value       = "ssh azureuser@${azurerm_public_ip.pip_running.ip_address}"
+  description = "Public IP of vm-destroyed (only set when create_vm_destroyed = true)"
+  value       = var.create_vm_destroyed ? azurerm_public_ip.pip_destroyed[0].ip_address : ""
 }
